74f326fe810336ed40af7d0edcd6d6ebd810df9f
Mandiegr <nannda076@gmail.com>
2024-08-26 23:39:12 -0300

    api: Add and integrate authentication and task routes

--- src/app.js
+++ src/app.js
@@ -5,9 +5,15 @@ const db = require('./config/db');
 
 require('../initializeDB');
 
+const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes')
+
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
